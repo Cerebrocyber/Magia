b69142273bb3099c96245f334e2ba60920491644
Cerebrocyber <cerebrocyber@gmail.com>
2025-08-06 19:51:16 -0300

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "M.A.G.I.A.",
   description: "Interface migrada com sucesso 🎉",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
